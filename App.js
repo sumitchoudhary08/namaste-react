a1a40187248bc68440cb9ada0308d01d4940535d
sumit <sumitbca93@gmail.com>
2025-04-01 16:44:46 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,43 @@
+// var heading = document.createElement("h1", {id:"heading"});
+// heading.innerHTML = "heading from app js!!";
+// document.getElementById("root").appendChild(heading);
+
+// var heading = React.createElement("h1", {id: "heading"}, "I am from react");
+// var root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+{/* <div id = "parent">
+    <div id="child1">
+        <h1>
+            h1 from child 1
+        </h1>
+        <h2>
+            h2 from child 1
+        </h2>
+    </div>
+    <div id="child2">
+        <h1>
+            h1 from child 2 
+        </h1>
+        <h2>
+            h2 from child 2
+        </h2>
+    </div>
+</div> */}
+
+var parent = React.createElement("div", {id:"parent"}, 
+    [React.createElement("div", {id:"child1"}, 
+        [React.createElement("h1", {}, "h1 from child 1")
+            , React.createElement("h2", {}, "h2 from child 1")
+        ]
+
+     ), 
+     React.createElement("div", {id:"child2"}, 
+        [React.createElement("h1", {}, "h1 from child 2")
+            , React.createElement("h2", {}, "h2 from child 2")
+        ]
+     ), 
+    ]
+);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
